a08ec03924d063d000193902d8e80c440ab70519
Echin-h <23050118@hdu.edu.cn>
2024-05-18 22:18:29 +0800

    feat : init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Iltmw
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// I love to memorize words
+// 这是我爱记单词的脚本
+
+// 智慧杭电登录，拿到一个token
+// token 去上课啦里面获取试卷
+// json 里面有所有的题目
+// 从题目里面提取出所有的单词，搜索到单词的意思
+// 匹配完之后呢，你自己去生成一个新的Jason，然后这个新的Jason里面是要有你所有的什么试卷什么东西。
+// 再调用提交的接口，把这个新的Json提交上去，就可以了。
+// 如果是电脑上做题目，就需要带上手机的user
+
+// https://github.com/hdulib/hdu
+
+func main() {
+	fmt.Println("Hello, World!")
+}
